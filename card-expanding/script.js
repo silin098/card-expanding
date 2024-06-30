89ab01ed90ff43994b7b6d2c0d91f3087ba9fe79
silin098 <dearjohn544@gmail.com>
2024-06-30 14:02:22 +0630

    initial push

---
+++ card-expanding/script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel')
+const color1 = document.querySelectorAll('.color1');
+
+panels.forEach(panel=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasses();
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+       
+    })
+}
+
+
+
